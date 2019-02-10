9c050277005ac40ff085bcc4b81f515e79aac28c
kingstonPLaw <47463048+kingstonPLaw@users.noreply.github.com>
2019-02-10 11:11:42 -0500

    Add files via upload
    This version includes a filter tab in the sidebar that enables filtering functionality of experiments 1 and 2 using the same parameters with the click of a button. The genes identified are counted and displayed on the dash. 
    
    The next update will add table visualization to the body of the data table tab.
    Adding additional keyword parameters significantly increases the filters run time. I will examine the possibility to allow users to add more keywords if they want.

--- Gene_Expression_Interaction_app.R
+++ Gene_Expression_Interaction_app.R
@@ -4,13 +4,13 @@ library(shinydashboard)
 
 options(shiny.maxRequestSize = 30*1024^2)
 
-skin <- "blue"
-
 header <- dashboardHeader(title = "Automated Analysis")
   
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
+    id = "tabs",
+    
     menuItem("Dashboard",
              tabName = "dashboard",
              icon = icon("tachometer-alt")),
@@ -39,15 +39,32 @@ sidebar <- dashboardSidebar(
     menuItem("Github Link",
              href = "https://github.com/kingstonPLaw/GeneExpression-Interactions",
              newtab = TRUE,
-             icon = icon("code"))
+             icon = icon("code")),
+    
+    menuItemOutput("filters")
   )
 )
   
   
 body <- dashboardBody(
   tabItems(
-    tabItem(tabName = "dashboard"
+    tabItem(tabName = "dashboard",
+      tags$hr(),
       
+      #genes Identified from filter
+      fluidRow(
+        valueBoxOutput("total_genes"),
+        valueBoxOutput("genes_experiment1"),
+        valueBoxOutput("genes_experiment2")),
+      
+      tags$hr(),
+      
+      # String Network
+      fluidRow(),
+      
+      tags$hr(),
+      
+      fluidRow()
     ),
     
     tabItem(tabName = "gene_info"
@@ -111,7 +128,7 @@ body <- dashboardBody(
                       buttonLabel = "Browse...",
                       placeholder = "No file selected"))),
       
-      tags$hr(),  
+      tags$hr(),
       
       # Experiment 2 Data Upload
       fluidRow(
@@ -127,7 +144,17 @@ body <- dashboardBody(
                              "text/comma-separated-values,text/plain",
                              ".csv"),
                       buttonLabel = "Browse...",
-                      placeholder = "No file selected")))
+                      placeholder = "No file selected"))),
+      
+      tags$hr(),
+      
+      # Action Button to Proceed to Dashboard
+      fluidRow(
+        infoBox(title = tags$b("Proceed to Dashboard"),
+                color = "light-blue",
+                icon = icon("tachometer-alt"),
+                actionButton(inputId = "jumptodash",
+                             label = "Go")))
     )
   )
 )
@@ -135,17 +162,144 @@ body <- dashboardBody(
 
 
 # Define UI for application -- arguments are defined above
-ui <- dashboardPage(skin = skin,
+ui <- dashboardPage(skin = "blue",
                     header = header,
                     sidebar = sidebar,
                     body = body)
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
   
+  scrape <- reactive({
+    paste0("(((", input$scrape1, "+AND+", input$scrape2, ")+AND+",
+              input$scrape3, ")+AND+", input$scrape4, ")")})
+  
+  # data to be used for EXPERIMENT 1 ANALYSIS:
+  experiment1_data <- eventReactive(input$filter_button,{
+    if (is.null(input$experiment1_file)) {
+      return(NULL)
+    } else {
+      exp1 <- as_tibble(read_csv(input$experiment1_file$datapath), na.rm = TRUE)
+      
+      # Define filter parameters
+      search <- paste(input$search1, input$search2,
+                      input$search3, input$search4, sep = "|")
+      
+      # Filter based on Inputs
+      experiment1_filt <- exp1 %>%
+        filter(grepl(search, exp1[,ncol(exp1)]))
+      experiment1_filt <- experiment1_filt %>%
+        filter(experiment1_filt[,2] > input$fc_val | experiment1_filt[,2] < -(input$fc_val))
+      experiment1_filtered <- experiment1_filt %>%
+        filter(experiment1_filt[,3] > input$ppde_val)
+      
+      return(experiment1_filtered)
+    }})
+  
+  # Data to be used for EXPERIMENT 2 ANALYSIS
+  experiment2_data <- eventReactive(input$filter_button,{
+    if (is.null(input$experiment2_file)) {
+      return(NULL)
+    } else {
+      exp2 <- as_tibble(read_csv(input$experiment2_file$datapath), na.rm = TRUE)
+      
+      # Define filter parameters
+      search <- paste(input$search1, input$search2,
+                      input$search3, input$search4, sep = "|")
+      
+      # Filter based on Inputs
+      experiment2_filt <- exp2 %>%
+        filter(grepl(search, exp2[,ncol(exp2)]))
+      experiment2_filt <- experiment2_filt %>%
+        filter(experiment2_filt[,2] > input$fc_val | experiment2_filt[,2] < -(input$fc_val))
+      experiment2_filtered <- experiment2_filt %>%
+        filter(experiment2_filt[,3] > input$ppde_val)
+      
+      return(experiment2_filtered)
+    }})
+  
+  # GENE counts for Dash:
+  gene1_count <- reactive({nrow(experiment1_data())})
+  gene2_count <- reactive({nrow(experiment2_data())})
+  total_gene_count <- reactive({gene1_count() + gene2_count()})
+  
+  # GENE lists to filter Mulitlevel data
+  gene1_list <- reactive({experiment1_data()[,1]})
+  gene2_list <- reactive({experiment2_data()[,1]})
+  
+  #Filter Mulitlevel
+  multilevel_data <- reactive({
+    if (is.null(input$multilevel_file)) {
+      return(NULL)
+    } else {
+      multi <- as_tibble(read_csv(input$multilevel_file$datapath), na.rm = TRUE)
+      
+      gene_list <- unique(rbind(gene1_list, gene2_list))
+      multilvl <- multi %>%
+        filter(X1 %in% gene_list$X1)
+      
+      return(multilvl)}
+  })
+        
+  
+  # When action button in upload tab selected, jump to dash
+  observeEvent(input$jumptodash, {
+    updateTabItems(session,
+                   inputId = "tabs",
+                   selected = "dashboard")})
+  
+  # Make Sliders for filtering data
+  output$filters <- renderMenu({
+    menuItem("FILTERS",
+             tabName = "filters",
+             icon = icon("filter"),
+             sliderInput(inputId = "ppde_val",
+                         label = "PPDE VALUE",
+                         min = 0,
+                         max = 1,
+                         value = 0.75,
+                         step = 0.01),
+             sliderInput(inputId = "fc_val",
+                         label = "FC ABSOLUTE VALUE",
+                         min = 0,
+                         max = 5,
+                         value = 1.5,
+                         step = 0.01),
+             textInput(inputId = "search1",
+                       label = "KEYWORD FILTER 1",
+                       value = ""),
+             textInput(inputId = "search2",
+                       label = "KEYWORD FILTER 2",
+                       value = ""),
+             textInput(inputId = "search3",
+                       label = "KEYWORD FILTER 3",
+                       value = ""),
+             textInput(inputId = "search4",
+                       label = "KEYWORD FILTER 4",
+                       value = ""),
+             actionButton(inputId = "filter_button",
+                          label = "FILTER"))})
+  
+  output$total_genes <- renderValueBox({
+    # Total genes identified from filtered data of experiments 1 and 2 
+    valueBox(value = total_gene_count(),
+             subtitle = tags$b("Total Genes Identified"),
+             icon = icon("dna"),
+             color = "light-blue")})
+  
+  output$genes_experiment1 <- renderValueBox({
+    # Genes identified from filtered data experiment 1
+    valueBox(value = gene1_count(),
+             subtitle = tags$b(paste0("Genes identified from experiment 1 (", input$experiment1_name, ")")),
+             color = "light-blue")})
+  
+  output$genes_experiment2 <- renderValueBox({
+    # Genes identified from filtered data experiment 2
+    valueBox(value = gene2_count(),
+             subtitle = tags$b(paste0("Genes identified from experiment 2 (", input$experiment2_name, ")")),
+             color = "light-blue")})
 }
                     
   
 # Run the application 
 shinyApp(ui = ui, server = server)
-
